e769d9d0d2e743366afce954cd1b9716919f9eef
mango dyl <dylanbonds@hotmail.com>
2019-02-06 15:31:17 -0600

    switch cases added

--- liri.js
+++ liri.js
@@ -2,9 +2,44 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 
-const command = process.argv[2];
-const input = process.argv[3];
+var argsArr = process.argv.slice(2);
+
+const command = argsArr.shift();
+const input = argsArr;
+
+switch (command) {
+  case 'spotify-this-song' :
+    spotify();
+    console.log("spotify");
+    break;
+  
+  case 'concert-this' :
+    bands();
+    console.log("concert");
+    break;
+
+  case 'movie-this' :
+    movie();
+    console.log("movie")
+    break;
+
+  case 'do-what-it-says' :
+    anyInput();
+    console.log("any");
+    break;
+}
 
 
+// Spotify API
 
-var spotify = new Spotify(keys.spotify);
+function spotify() {
+  var spotify = new Spotify(keys.spotify);
+
+  spotify.search({ type: 'track', query: input }, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+    
+    console.log(data); 
+  });
+};
